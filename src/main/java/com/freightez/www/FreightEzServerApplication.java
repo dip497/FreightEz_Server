62ff045507e787ddfa9a12fb48917dc0a9f9b588
dipendra sharma <dipendra497@gmail.com>
2023-06-08 19:40:24 +0530

    init

---
+++ src/main/java/com/freightez/www/FreightEzServerApplication.java
@@ -0,0 +1,13 @@
+package com.freightez.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightEzServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreightEzServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freightez/www/FreightEzServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freightez.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreightEzServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
